5a03c8dc98e4306f9d6e3bff6f2edccd0330665d
emendietaj <emendietaj@gmail.com>
2016-02-13 23:39:51 -0500

    changed title and styled with css and jquery fittext plugin

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
         <![endif]-->
 
     <div class="container">
-      <h1><?php echo $PAGE_TITLE; ?></h1>
+      <h1 class="title"><?php echo $PAGE_TITLE; ?></h1>
       <!-- Example row of columns -->
       <ul class="photo-gallery">
         <?php
@@ -58,7 +58,7 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
+        <script src="js/jquery.fittext.js"</script>
         <script src="js/vendor/bootstrap.min.js"></script>
 
         <script src="js/main.js"></script>
